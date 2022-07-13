9b112e37620c3731f9705c91de6c866c842f925c
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2022-07-13 16:48:48 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const path = require('path');
 const express = require('express');
 const app = express();
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const contactRouter = require('./routes/contactRoutes');
 const userRouter = require('./routes/userRoutes');
 const viewRouter = require('./routes/viewRoutes');
 const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
+// const helmet = require('helmet');
+// const mongoSanitize = require('express-mongo-sanitize');
+// const xss = require('xss-clean');
+// const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const html = require('html');
+
 //MIDDLEWARES
 
 //Middleware(stands between the request and the response). Thanks to the middleware below(express.json()),
@@ -20,7 +22,7 @@ const cookieParser = require('cookie-parser');
 app.use(express.json());
 //We said app.use(), so express.json() is added to the middleware stack
 
-//Creating our own middleware
+// Creating our own middleware
 app.use((req, res, next) => {
   //When we passed the (rex,res,next), express knows that we define a middleware function
   console.log('Hello from the middleware ☀️');
@@ -29,14 +31,14 @@ app.use((req, res, next) => {
   next();
 });
 
-//Viewer
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'views')));
+// //Viewer
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'assets')));
+
+app.set('view engine', 'ejs');
 //Set Security http headers
-app.use(helmet());
+// app.use(helmet());
 if (process.env.NODE_ENV === 'development') {
   //app.use(morgan('tiny'));
 } //morgan('tiny') returns a function }
@@ -47,13 +49,15 @@ const limiter = rateLimit({
   windowMs: 60 * 60 * 1000,
   message: 'Too many request from this IP, please try again later',
 });
-
-app.use('/api', limiter);
-
-app.use(mongoSanitize());
-
-app.use(xss());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use('/api', limiter);
+
+// app.use(mongoSanitize());
+
+// app.use(xss());
+
 app.use(cookieParser());
 
 //Routes
@@ -61,13 +65,13 @@ app.use(cookieParser());
 app.use('/', viewRouter);
 app.use('/api/v1/contacts', contactRouter);
 app.use('/api/v1/users', userRouter);
-
 app.all('*', (req, res, next) => {
-  //   const err = new Error(`Can't find ${req.originalUrl} on this server`);
-  //   err.status = 'fail';
-  //   err.statusCode = 404;
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
 
-  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+  next();
+  new AppError(`Can't find ${req.originalUrl} on this server`, 404);
 });
 
 app.use(globalErrorHandler);
