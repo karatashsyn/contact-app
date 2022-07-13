b6201db22acf57116453660bde30ce73ab01ec90
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2022-07-13 16:40:26 +0300

    Update contactController.js

--- Controllers/contactController.js
+++ Controllers/contactController.js
@@ -3,23 +3,6 @@ const AppError = require('../utils/appError');
 const Contact = require('./../models/contactModel');
 const catchAsync = require('./../utils/catchAsync');
 
-exports.getAllContacts = catchAsync(async (req, res, next) => {
-  const queryObject = { ...req.query };
-  const excludedFields = ['page', 'sort', 'limit', 'fields'];
-  excludedFields.forEach((el) => delete queryObject[el]);
-  //console.log(req.query, queryObject);
-  const query = Contact.find(queryObject);
-  ////SORTING PAGINATION.. OTHER THINGS(WE DIDN'T AWAITED CONTACTS IMMEDIATELY BECAUSE WE ARE GONNA IMPLEMENT THE SORTING, PAGINATION AND OTHER STUFF)
-  const allContacts = await query;
-  res.status(200).json({
-    status: 'success',
-    results: allContacts.length,
-    data: {
-      allContacts,
-    },
-  });
-});
-
 exports.getContact = catchAsync(async (req, res, next) => {
   const contact = await Contact.findById(req.params.id);
   //Contact.findOne({_id:req.params.id})
@@ -33,14 +16,24 @@ exports.getContact = catchAsync(async (req, res, next) => {
   });
 });
 
+exports.getAllContacts = catchAsync(async (req, res, next) => {
+  const contacts = await Contact.find().populate('contacts');
+  res.status(200).json({
+    status: 'success',
+    data: contacts,
+  });
+});
+
 exports.createContact = catchAsync(async (req, res, next) => {
   // const newContact = new Contact({});
   // newContact.save();
-  const newContact = await Contact.create(req.body); //same functionality with the 2 lines above.
-
+  console.log(req.body); //This includes name: Huseyin Karatas and
+  const newContact = await Contact.create({ ...req.body, currentId: null });
+  req.body = { ...req.body, newContactId: newContact._id };
   res.status(201).json({
     status: 'success',
     data: {
+      user: req.user,
       contact: newContact,
     },
   });
