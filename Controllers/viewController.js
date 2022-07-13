211c58a39bd44e711f38964b2b163d8960fd2f19
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2022-07-13 16:42:31 +0300

    Update viewController.js

--- Controllers/viewController.js
+++ Controllers/viewController.js
@@ -1,29 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const Contact = require('../models/contactModel');
 const catchAsync = require('../utils/catchAsync');
 const slug = require('slugify');
 
-exports.getOverView = async (req, res) => {
-  const contacts = await Contact.find();
+// exports.getOverView = async (req, res) => {
+//   const contacts = await Contact.find();
+//   res.status(200).sendFile(path.join(__dirname, '/overview.html'));
+// };
 
-  res.status(200).render('contacts', {
-    title: 'contacts',
-    contacts,
-  });
-};
+// exports.getContact = async (req, res) => {
+//   const contact = await Contact.findById(req.params.id);
 
-exports.getContact = async (req, res) => {
-  const contact = await Contact.findById(req.params.id);
-
-  res.status(200).render('contactProfile', {
-    title: contact.name,
-    contact,
-  });
-};
+//   res.sendFile(path.join(__dirname, '/contactProfile.html'));
+// };
 
 exports.getLoginForm = (req, res) => {
-  res.status(200).render('loginScreen.ejs', {
-    title: 'Log in',
-  });
+  res.status(200).render('loginScreen');
 };
+
+exports.getOverView = (req, res) => {
+  res.status(200).render('overView');
+};
+
+exports.getSignUpForm = (req, res) => {
+  res.status(200).render('signUp');
+};
+//For html
+// exports.getContactProfile = (req, res) => {
+//   res.status(200).sendFile(path.join(__dirname, '/contactProfile.html'));
+// };
+exports.getAddContact = (req, res) => {
+  res.status(200).render('addContact');
+};
+
+exports.getEditContact = (req, res) => {
+  res.status(200).render('editContact');
+};
+// exports.getEditContact = (req, res) => {
+//   res.status(200).sendFile(path.join(__dirname, '/editContact.html'));
+// };
