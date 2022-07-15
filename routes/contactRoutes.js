4dff49e9eb216ad198200855a8fa51e84c6298eb
karatashsyn <81769362+karatashsyn@users.noreply.github.com>
2022-07-15 12:42:43 +0300

    Update contactRoutes.js

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -13,16 +13,5 @@ router.param('id', (req, res, next, val) => {
   console.log(`Tour id is ${val}`);
   next();
 });
-// router.get(
-//   '/allcontacts',
-//   authController.isLoggedIn,
-//   contactController.getAllContacts
-// );
-//When there is a post request, first checkBody, then createContact function
-// will be called.=> Chaining multiple middlewares.
-// router
-//   .route('/:id')
-//   .get(contactController.getContact)
-//   .patch(contactController.updateContact)
-//   .delete(contactController.deleteContact);
+
 module.exports = router;
